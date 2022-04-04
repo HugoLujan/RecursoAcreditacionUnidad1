fcee22d983c95dad218a2ea4f0311be096bb0740
= <huedlup@gmail.com>
2022-04-04 11:58:41 -0600

    recurso

---
+++ src/components/Foco.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import {Image, View, Button} from 'react-native';
+
+const foco = () => {
+
+  const [estatus, setEstatus] = useState(require('../../img/focoOff.png'));
+
+
+  return (
+    <View>
+      
+        <Image source= {estatus} 
+        style={{width: 80, height: 130}}> 
+        </Image>
+        <Button title="Apagar" color="gray" onPress={ ()=> setEstatus(require('../../img/focoOff.png'))}></Button>
+        <Button title="Amarillo" color="orange" onPress={ ()=> setEstatus(require('../../img/focoOn.png'))}></Button>
+        <Button title="Rojo" color="red" onPress={ ()=> setEstatus(require('../../img/focoRojo.png'))}></Button>
+        <Button title="Azul" color="blue" onPress={ ()=> setEstatus(require('../../img/focoAzul.png'))}></Button>
+        <Button title="Verde" color="green" onPress={ ()=> setEstatus(require('../../img/focoVerde.png'))}></Button>
+    </View>
+    
+  )
+}
+
+export default foco
